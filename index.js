a02be0a03b6ddf00e00e684a1f7b2d80fd32a95c
sergiodeveloper19 <sergio.motril1993@gmail.com>
2022-10-22 11:11:07 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+function getCharacter (done){
+    
+    const results = fetch ("https://rickandmortyapi.com/api/character");
+
+    results
+        .then(response => response.json())
+        .then(data => {
+            done(data)
+        });
+
+}
+
+getCharacter(data => {
+    data.results.forEach(personaje => {
+        const article = document.createRange().createContextualFragment(/*html*/`
+
+        <article>
+            <div class="image-container">
+                <img src="${personaje.image}" alt="Personaje">
+            </div>
+        
+            <h2>${personaje.name}</h2>
+        
+            <span>${personaje.status}</span>
+
+            <br>
+        
+        </article>
+
+        
+        `);
+        const main = document.querySelector("main");
+        main.append(article);
+    });
+});
